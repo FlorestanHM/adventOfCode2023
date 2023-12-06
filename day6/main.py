f633136f6225065592fce298630c2f6ea4e2d38c
Florestan HM <fhacquesminier@asi.fr>
2023-12-06 12:17:41 +0100

    day6 part2

--- day6/main.py
+++ day6/main.py
@@ -10,24 +10,23 @@ def get_data():
     line0 = lines[0].split()
     line1 = lines[1].split()
 
-    data = [[line0[i], line1[i]] for i in range(0, len(line0))]
-    return data[1:]
+    data = [[line0[i], line1[i]] for i in range(0, len(line0))][1:]
+
+    return [''.join([course[0] for course in data]), ''.join([course[1] for course in data])]
 
 
 def main():
-    courses = get_data()
-    mult = 1
-    for course in courses:
-        duree_course = int(course[0])
-        distance_battre = int(course[1])
+    course = get_data()
+    duree_course = int(course[0])
+    distance_battre = int(course[1])
 
-        nombre_possibilite = 0
-        for i in range(0, duree_course + 1):
-            distance_parcouru = (duree_course - i)  * i
-            if distance_parcouru > distance_battre:
-                nombre_possibilite += 1
-        mult *= nombre_possibilite
-    print(mult)
+    nombre_possibilite = 0
+    for i in range(0, duree_course + 1):
+        distance_parcouru = (duree_course - i)  * i
+        if distance_parcouru > distance_battre:
+            nombre_possibilite += 1
+
+    print(nombre_possibilite)
 
 
 
